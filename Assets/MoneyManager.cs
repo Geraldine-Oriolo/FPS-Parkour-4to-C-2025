15006f08bdc854d91e3377c9f8b1021bc98b316b
Geraldine-Oriolo <49374998@est.ort.edu.ar>
2025-06-05 11:40:51 -0300

    Cambios en el repo de jero

---
+++ Assets/MoneyManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    public float money;
+    public UIMANAGER uiManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void UpdateMoney(float moneyToAdd)
+    {
+        money += moneyToAdd;
+    }
+}
